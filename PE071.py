c6818a8dfb868d535d1b9c4529dc3ca867097302
deadbot74 <sourav.sur74@gmail.com>
2019-05-26 02:56:38 +0530

    no code, just basic math and some logic

---
+++ PE071.py
@@ -0,0 +1,7 @@
+print(3 / 7)
+#  since 428571/1000000 = 3/7
+#  As we know, higher the numerator, more is the precision
+#  Since numerator = 428571 gives exactly 3/7. We need the one on the closest left of it
+#  Thus numerator = 428570 is the plausible answer
+print(428571 / 999999)
+print(428570 / 999999)
